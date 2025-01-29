dbcf28b6fe04f0421b9d454477f78ab7b484694e
Fatih Burak Toprak <fatihburaktoprak@gmail.com>
2025-01-29 11:14:23 +0300

    Create transfer.ps1

---
+++ transfer.ps1
@@ -0,0 +1,13 @@
+﻿$file = Get-ChildItem -Path "C:\eren\" -Filter "*.vhdx" -Recurse
+
+if ($file) {
+    $vhdxFileName = $file.FullName  # Get the full path of the file
+
+    $destinationPath = "O:\ortak_haftalik\RedTeam"
+
+    Copy-Item -Path $vhdxFileName -Destination $destinationPath
+
+    Write-Output "Copied to $destinationPath"
+} else {
+    Write-Output ".vhdx not found."
+}
