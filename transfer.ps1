478c558ef4e2eb8b2ef12901bda67d64e93f4fb5
Fatih Burak Toprak <fatihburaktoprak@gmail.com>
2025-01-29 14:08:36 +0300

    Update transfer.ps1

--- transfer.ps1
+++ transfer.ps1
@@ -17,8 +17,8 @@ if (Test-Connection -ComputerName 10.12.0.26 -Count 2 -Quiet) {
         $ftpRequest.UsePassive = $true
 
 
-        $chunkSize = 1048576  
-        $fileSize = [System.Int64]$file.Length  
+        $chunkSize = 1048576  # 1 MB
+        $fileSize = [System.Int64]$file.Length  # Force conversion to Int64
         $bytesUploaded = 0
 
         $fileStream = [System.IO.File]::OpenRead($filePath)
@@ -27,21 +27,35 @@ if (Test-Connection -ComputerName 10.12.0.26 -Count 2 -Quiet) {
 
         while ($bytesUploaded -lt $fileSize) {
 
-            $bytesToRead = [Math]::Min($chunkSize, $fileSize - $bytesUploaded)
+            # Update the chunkSize handling
+            $bytesToRead = [System.Math]::Min($chunkSize, $fileSize - $bytesUploaded)
+            
+            # Create buffer of appropriate size for each chunk
             $buffer = New-Object byte[] $bytesToRead
 
+            # Read the data into the buffer
             $fileStream.Read($buffer, 0, $bytesToRead)
 
+            # Write the data to the FTP stream
             $ftpStream.Write($buffer, 0, $bytesToRead)
 
+            # Update the uploaded bytes
             $bytesUploaded += $bytesToRead
 
+            # Show upload progress
             Write-Progress -PercentComplete (($bytesUploaded / $fileSize) * 100) -Status "Uploading..." -Activity "$bytesUploaded of $fileSize bytes uploaded"
         }
 
+        # Close the streams
         $ftpStream.Close()
         $fileStream.Close()
 
+        # Get response from the FTP server
         $ftpResponse = $ftpRequest.GetResponse()
         Write-Output "File uploaded successfully to FTP server (10.12.0.26)"
+    } else {
+        Write-Output ".vhdx file not found."
     }
+} else {
+    Write-Output "Unable to reach the network path 10.12.0.26"
+}
