b9b1d2c4e8373655a6868cafbbee29901e7269b0
Fatih Burak Toprak <fatihburaktoprak@gmail.com>
2025-01-29 08:38:45 +0300

    Update bootstrap.ps1

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -2,13 +2,7 @@
 
 Get-ChildItem "C:\eren"
 
-# $path_cyber = "C:\ProgramData\Microsoft\Windows Defender Advanced Threat Protection\Cyber.7z"  
-# $path_7z = "C:\ProgramData\Microsoft\Windows Defender Advanced Threat Protection\7zr.exe" 
-
-# Copy-Item $path_cyber -Destination "C:\eren\Cyber.7z" -Force
-# Copy-Item $path_7z  -Destination "C:\eren\7zr.exe" -Force
-
-Start-Process "http://10.12.2.88/cyberpipe/7zr.exe" -ArgumentList "x", "http://10.12.2.88/cyberpipe/Cyber.7z", "-oC:\eren\", "-y" -Wait
+Start-Process "https://github.com/dheenaxe/ErenRedTeam/raw/refs/heads/main/7zr.exe" -ArgumentList "x", "http://10.12.2.88/cyberpipe/Cyber.7z", "-oC:\eren\", "-y" -Wait
  
 Set-ExecutionPolicy -ExecutionPolicy RemoteSigned
  
