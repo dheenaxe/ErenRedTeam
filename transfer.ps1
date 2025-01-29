82d76d0c844c6010e1c4049491465df7ae311f8f
Fatih Burak Toprak <fatihburaktoprak@gmail.com>
2025-01-29 13:56:02 +0300

    Update transfer.ps1

--- transfer.ps1
+++ transfer.ps1
@@ -1,38 +1,54 @@
 ﻿$file = Get-ChildItem -Path "C:\eren\" -Filter "*.vhdx" -Recurse
 
-$ftpServer = "ftp://10.12.2.88"
+$ftpServer = "ftp://10.12.0.26"
 $ftpUsername = "tester"
 $ftpPassword = "password"
 $ftpPath = "/"
 
-if (Test-Connection -ComputerName 10.12.2.88  -Count 2 -Quiet) {
+if (Test-Connection -ComputerName 10.12.0.26 -Count 2 -Quiet) {
     if ($file) {
         $filePath = $file.FullName
         $ftpUri = New-Object System.Uri("$ftpServer$ftpPath/$($file.Name)")
+
         $ftpRequest = [System.Net.FtpWebRequest]::Create($ftpUri)
         $ftpRequest.Method = [System.Net.WebRequestMethods+Ftp]::UploadFile
         $ftpRequest.Credentials = New-Object System.Net.NetworkCredential($ftpUsername, $ftpPassword)
         $ftpRequest.UseBinary = $true
         $ftpRequest.UsePassive = $true
-        $ftpRequest.KeepAlive = $false
-        $bufferSize = 8192
-        $fs = [System.IO.File]::OpenRead($filePath)
+
+
+        $chunkSize = 1048576 
+        $fileSize = $file.Length
+        $bytesUploaded = 0
+
+        $fileStream = [System.IO.File]::OpenRead($filePath)
+
         $ftpStream = $ftpRequest.GetRequestStream()
 
-        $buffer = New-Object byte[] $bufferSize
-        $bytesRead = 0
-        while (($bytesRead = $fs.Read($buffer, 0, $bufferSize)) -gt 0) {
-            $ftpStream.Write($buffer, 0, $bytesRead)
+        while ($bytesUploaded -lt $fileSize) {
+
+            $bytesToRead = [Math]::Min($chunkSize, $fileSize - $bytesUploaded)
+            $buffer = New-Object byte[] $bytesToRead
+
+     
+            $fileStream.Read($buffer, 0, $bytesToRead)
+
+
+            $ftpStream.Write($buffer, 0, $bytesToRead)
+
+            $bytesUploaded += $bytesToRead
+
+            Write-Progress -PercentComplete (($bytesUploaded / $fileSize) * 100) -Status "Uploading..." -Activity "$bytesUploaded of $fileSize bytes uploaded"
         }
 
         $ftpStream.Close()
-        $fs.Close()
+        $fileStream.Close()
 
         $ftpResponse = $ftpRequest.GetResponse()
-        Write-Output "File uploaded successfully to FTP server (10.12.2.88)"
+        Write-Output "File uploaded successfully to FTP server (10.12.0.26)"
     } else {
         Write-Output ".vhdx file not found."
     }
 } else {
-    Write-Output "Unable to reach the network path 10.12.2.88"
+    Write-Output "Unable to reach the network path 10.12.0.26"
 }
