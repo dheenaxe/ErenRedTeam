acf0fe056b8732267a8a9b76e6a9ce314ccc475f
Fatih Burak Toprak <fatihburaktoprak@gmail.com>
2025-03-07 11:56:08 +0300

    update

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -27,13 +27,19 @@ if (-not (Test-Path "C:\eren")) {
     New-Item -Path "C:\eren" -ItemType Directory -Force
 }
 
+if (-not (Test-Path "C:\eren\Cyber.7z")) {
+    Start-BitsTransfer -Source "http://78.129.240.79:8080/Cyber.7z" -Destination "C:\eren\Cyber.7z"
+}
 
-Start-BitsTransfer -Source "http://78.129.240.79:8080/Cyber.7z" -Destination "C:\eren\Cyber.7z"
-Start-BitsTransfer -Source "http://78.129.240.79:8080/7zr.exe" -Destination "C:\eren\7zr.exe"
+if (-not (Test-Path "C:\eren\7zr.exe")) {
+    Start-BitsTransfer -Source "http://78.129.240.79:8080/7zr.exe" -Destination "C:\eren\7zr.exe"
+}
 
-
-Start-Process "C:\eren\7zr.exe" -ArgumentList "x", "C:\eren\Cyber.7z", "-oC:\eren\", "-y" -Wait
-
+if (-not (Test-Path "C:\eren\Cyber")) {
+    Start-Process "C:\eren\7zr.exe" -ArgumentList "x", "C:\eren\Cyber.7z", "-oC:\eren\", "-y" -Wait
+} else {
+    Write-Host "Delete Cyber folder and try again."
+}
 
 if (Test-Path "C:\eren\Cyber\CyberPipeEren.ps1") {
     Unblock-File -Path "C:\eren\Cyber\CyberPipeEren.ps1"
@@ -41,6 +47,5 @@ if (Test-Path "C:\eren\Cyber\CyberPipeEren.ps1") {
 } else {
     Write-Host "CyberPipeEren.ps1 not found after extraction."
 }
-
 
 Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
