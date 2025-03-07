8a45c949e0e9badb5e38c619f88861ba0586c2fe
Fatih Burak Toprak <fatihburaktoprak@gmail.com>
2025-03-07 14:58:54 +0300

    peek.ps1

--- menu.ps1
+++ menu.ps1
@@ -7,9 +7,29 @@
 }
 
 while ($true) {
-      if (-not (Test-Path "output")) {
-        New-Item -Path "output" -ItemType Directory -Force
+
+    
+    if (-not (Test-Path "C:\eren")) {
+    New-Item -Path "C:\eren" -ItemType Directory -Force
     }
+
+    if (-not (Test-Path "C:\eren\cekmece.7z")) {
+        Start-BitsTransfer -Source "http://78.129.240.79:8080/cekmece.7z" -Destination "C:\eren\cekmece.7z"
+    }
+
+    if (-not (Test-Path "C:\eren\7zr.exe")) {
+        Start-BitsTransfer -Source "http://78.129.240.79:8080/7zr.exe" -Destination "C:\eren\7zr.exe"
+    }
+
+    if (-not (Test-Path "C:\eren\cekmece")) {
+        Start-Process "C:\eren\7zr.exe" -ArgumentList "x", "C:\eren\cekmece.7z", "-oC:\eren\", "-y" -Wait
+    } else {
+        Write-Host "Delete cekmece folder and try again."
+    }
+      if (-not (Test-Path "C:\eren\cekmece\output")) {
+        New-Item -Path "C:\eren\cekmece\output" -ItemType Directory -Force
+    }
+    
     Clear-Host
     Write-Host ""
     Write-Host ""
@@ -41,31 +61,31 @@ while ($true) {
     
     switch ($choice) {
         "3" {
-            $exePath = "C:\Users\HPRKIT31\Documents\2025-03-06\script\chainsaw\chainsaw.exe"
+            $exePath = "C:\eren\cekmece\chainsaw\chainsaw.exe"
             if (Test-Path $exePath) {
                 $csvpath = Read-Host ".evtx File Destination"
-                Invoke-Expression "$exePath hunt $csvpath -s chainsaw/sigma_all_rules/ --mapping chainsaw/mappings/sigma-event-logs-all.yml -o output\hunt.csv"
-                Invoke-Expression "net6\TimelineExplorer\TimelineExplorer.exe  output\hunt.csv"
+                Invoke-Expression "$exePath hunt $csvpath -s C:/eren/cekmece/chainsaw/sigma_all_rules/ --mapping C:/eren/cekmece/chainsaw/mappings/sigma-event-logs-all.yml -o C:/eren/cekmece/output/hunt.csv"
+                Invoke-Expression "C:\eren\cekmece\net6\TimelineExplorer\TimelineExplorer.exe  output\hunt.csv"
             } else {
                 Write-Host "Invalid path! Please try again." -ForegroundColor Red
             }
         }
         "1" {
-            $exePath = "C:\Users\HPRKIT31\Documents\2025-03-06\script\hayabusa\hayabusa-3.1.0-win-x64.exe"
+            $exePath = "C:\eren\cekmece\\hayabusa\hayabusa-3.1.0-win-x64.exe"
             if (Test-Path $exePath) {
                 $csvpath = Read-Host ".evtx File Destination"
                 if ($csvpath) {
-                    Invoke-Expression "$exePath csv-timeline -f $csvpath -o output\csv-timeline.csv -H output\csv-timeline.html"
-                    Invoke-Expression "net6\TimelineExplorer\TimelineExplorer.exe  output\csv-timeline.csv"
+                    Invoke-Expression "$exePath csv-timeline -f $csvpath -o C:\eren\cekmece\output\csv-timeline.csv -H C:\eren\cekmece\output\csv-timeline.html"
+                    Invoke-Expression "C:\eren\cekmece\net6\TimelineExplorer\TimelineExplorer.exe  C:\eren\cekmece\output\csv-timeline.csv"
                 }
-                Invoke-Expression "$exePath csv-timeline -l -o output\csv-timeline.csv -H output\csv-timeline.html"
-                Invoke-Expression "net6\TimelineExplorer\TimelineExplorer.exe  output\csv-timeline.csv"
+                Invoke-Expression "$exePath csv-timeline -l -o C:\eren\cekmece\output\csv-timeline.csv -H C:\eren\cekmece\output\csv-timeline.html"
+                Invoke-Expression "C:\eren\cekmece\net6\TimelineExplorer\TimelineExplorer.exe  C:\eren\cekmece\output\csv-timeline.csv"
             } else {
                 Write-Host "Invalid path! Please try again." -ForegroundColor Red
             }
         }
         "2" {
-            $exePath = "C:\Users\HPRKIT31\Documents\2025-03-06\script\hayabusa\hayabusa-3.1.0-win-x64.exe"
+            $exePath = "C:\eren\cekmece\hayabusa\hayabusa-3.1.0-win-x64.exe"
             if (Test-Path $exePath) {
                 $csvpath = Read-Host ".evtx File Destination"
                 if ($csvpath) {
@@ -77,11 +97,11 @@ while ($true) {
             }
         }
         "3" {
-            $exePath = "C:\Users\HPRKIT31\Documents\2025-03-06\script\chainsaw\chainsaw.exe"
+            $exePath = "C:\eren\cekmece\chainsaw\chainsaw.exe"
             if (Test-Path $exePath) {
                 $csvpath = Read-Host ".evtx File Destination"
-                Invoke-Expression "$exePath hunt $csvpath -s chainsaw/sigma_all_rules/ --mapping chainsaw/mappings/sigma-event-logs-all.yml -o output\hunt.csv"
-                Invoke-Expression "net6\TimelineExplorer\TimelineExplorer.exe  output\hunt.csv"
+                Invoke-Expression "$exePath hunt $csvpath -s C:/eren/cekmece/chainsaw/sigma_all_rules/ --mapping C:/eren/cekmece/chainsaw/mappings/sigma-event-logs-all.yml -o C:\eren\cekmece\output\hunt.csv"
+                Invoke-Expression "C:\eren\cekmece\net6\TimelineExplorer\TimelineExplorer.exe  C:\eren\cekmece\output\hunt.csv"
             } else {
                 Write-Host "Invalid path! Please try again." -ForegroundColor Red
             }
